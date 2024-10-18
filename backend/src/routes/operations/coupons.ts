71ddf22abe4efc8eb08c231237fd203bc3d0d1f7
elizalsmn <elizalesmana88@gmail.com>
2024-10-18 13:07:13 +0800

    dummy data for coupons

--- backend/src/routes/operations/coupons.ts
+++ backend/src/routes/operations/coupons.ts
@@ -21,4 +21,65 @@ couponRouter.post("/coupons", verifyToken, multerUpload.any(), async (req: any, 
   }
 });
 
+couponRouter.post("/coupons/dummy", verifyToken, multerUpload.any(), async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const coupons: Coupon[] = [
+        {
+          coupon_id: "welcome_coupon",
+          description: "15% off for your first pet care in any of our partners",
+          price: 0,
+          value: 200
+        },
+        {
+          coupon_id: "mini_coupon",
+          description: "15hkd off, points required: 15 points",
+          price: 15,
+          value: 15
+        },
+        {
+          coupon_id: "medium_coupon_1",
+          description: "50hkd off, points required: 45 points",
+          price: 45,
+          value: 50
+        },
+        {
+          coupon_id: "medium_coupon_2",
+          description: "50hkd off, points required: 45 points",
+          price: 45,
+          value: 50
+        },
+        {
+            coupon_id: "vaccination_coupon",
+            description: "Discount pet vaccination for first-time users",
+            price: 20,
+            value: 50
+          },
+          {
+            coupon_id: "health_check_coupon",
+            description: "25% (max 80hkd) off on a comprehensive pet health check",
+            price: 20,
+            value: 80
+          },
+          {
+            coupon_id: "adoption_assistance_coupon",
+            description: "50% (max 100hkd) off adoption fees for any pet",
+            price: 50,
+            value: 100
+          }
+      ];
+  
+      let results = [];
+      for (let i = 0; i < 5; i++) {
+        for (let coupon of coupons) {
+          let result = await addCoupon(coupon);
+          results.push(result);
+        }
+      }
+      res.status(201).json({ message: "Coupons added successfully", results });
+    } catch (error: any) {
+      res.status(500).json({ message: "Error occurred", error });
+    }
+  });
+  
+
 export default couponRouter;
