ae3b1c495c65913ad91c1de4165379299eda1797
XUjiayi777 <jxugl777@gmail.com>
2024-10-18 13:09:42 +0800

    update

---
+++ frontend-main/src/views/admin/profile/components/Donation.jsx
@@ -0,0 +1,24 @@
+import Card from "components/card";
+import React from "react";
+
+const Donation = () => {
+  return (
+    <Card extra={"w-full h-full p-3"}>
+      {/* Header */}
+      <div className="mt-2 mb-8 w-full">
+        <h4 className="px-2 text-xl font-bold text-navy-700 dark:text-white">
+          Your Donation
+        </h4>
+        <p className="mt-2 px-2 text-base text-gray-600">
+        Your support today can make a lasting impact. Every contribution, big or small, brings us one step closer to animal rescue. 
+        Together, we can change lives, create hope, and make a difference.
+        </p>
+      </div>
+      {/* Cards */}
+      <div className="grid grid-cols-2 gap-4 px-2">
+      </div>
+    </Card>
+  );
+};
+
+export default Donation;
--- frontend-main/src/views/admin/profile/components/Project.jsx
+++ frontend-main/src/views/admin/profile/components/Project.jsx
@@ -4,45 +4,53 @@ import image1 from "assets/img/profile/image1.png";
 import image2 from "assets/img/profile/image2.png";
 import image3 from "assets/img/profile/image3.png";
 import Card from "components/card";
+import NftCard from "components/card/NftCard";
+import NFt2 from "assets/img/nfts/Nft2.png";
+import NFt4 from "assets/img/nfts/Nft4.png";
+import NFt3 from "assets/img/nfts/Nft3.png";
+import NFt5 from "assets/img/nfts/Nft5.png";
+import NFt6 from "assets/img/nfts/Nft6.png";
+import avatar1 from "assets/img/avatars/avatar1.png";
+import avatar2 from "assets/img/avatars/avatar2.png";
+import avatar3 from "assets/img/avatars/avatar3.png";
 
 const Project = () => {
   return (
     <Card extra={"w-full p-4 h-full"}>
       <div className="mb-8 w-full">
         <h4 className="text-xl font-bold text-navy-700 dark:text-white">
-          All projects
+          E-shops
         </h4>
         <p className="mt-2 text-base text-gray-600">
           Here you can find more details about your projects. Keep you user
           engaged by providing meaningful information.
         </p>
       </div>
-      {/* Project 1 */}
-      <div className="flex w-full items-center justify-between rounded-2xl bg-white p-3 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-        <div className="flex items-center">
-          <div className="">
-            <img className="h-[83px] w-[83px] rounded-lg" src={image1} alt="" />
-          </div>
-          <div className="ml-4">
-            <p className="text-base font-medium text-navy-700 dark:text-white">
-              Technology behind the Blockchain
-            </p>
-            <p className="mt-2 text-sm text-gray-600">
-              Project #1 .
-              <a
-                className="ml-1 font-medium text-brand-500 hover:text-brand-500 dark:text-white"
-                href=" "
-              >
-                See product details
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="mr-4 flex items-center justify-center text-gray-600 dark:text-white">
-          <MdModeEditOutline />
-        </div>
+      <div className="z-20 grid grid-cols-1 gap-5 md:grid-cols-3">
+      <NftCard
+            bidders={[avatar1, avatar2, avatar3]}
+            title="Abstract Colors"
+            author="Esthera Jackson"
+            price="0.91"
+            image={NFt3}
+          />
+          <NftCard
+            bidders={[avatar1, avatar2, avatar3]}
+            title="ETH AI Brain"
+            author="Nick Wilson"
+            price="0.7"
+            image={NFt2}
+          />
+          <NftCard
+            bidders={[avatar1, avatar2, avatar3]}
+            title="Mesh Gradients"
+            author="Will Smith"
+            price="2.91"
+            image={NFt4}
+          />
       </div>
-      {/* Project 1 */}
+
+{/* 
       <div className="mt-3 flex w-full items-center justify-between rounded-2xl bg-white p-3 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
         <div className="flex items-center">
           <div className="">
@@ -67,7 +75,7 @@ const Project = () => {
           <MdModeEditOutline />
         </div>
       </div>
-      {/* Project 1 */}
+
       <div className="mt-3 flex w-full items-center justify-between rounded-2xl bg-white p-3 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
         <div className="flex items-center">
           <div className="">
@@ -91,7 +99,7 @@ const Project = () => {
         <div className="mr-4 flex items-center justify-center text-gray-600 dark:text-white">
           <MdModeEditOutline />
         </div>
-      </div>
+      </div> */}
     </Card>
   );
 };
